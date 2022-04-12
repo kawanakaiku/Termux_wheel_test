ea7ec2e24c876d1ac6754338ab5def47321cc256
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-04-12 13:20:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="パッケージの名前",
-    version="パッケージのバージョン(例:1.0.0)",
+    name="simple_package",
+    version="1.0.0",
     install_requires=["packageA", "packageB"],
     extras_require={
         "develop": ["dev-packageA", "dev-packageB"]
